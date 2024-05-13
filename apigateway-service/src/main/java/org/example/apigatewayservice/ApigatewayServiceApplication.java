d342dd856fb0c406d65fefe0d9cc0efa1a533982
junhyungjang <jsy3535@dgist.ac.kr>
2024-05-14 00:30:20 +0900

    feat : apigateway conneection

---
+++ apigateway-service/src/main/java/org/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package org.example.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+@EnableDiscoveryClient
+public class ApigatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayServiceApplication.class, args);
+	}
+
+}
